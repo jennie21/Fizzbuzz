aaf4d3bc8d7db9886429e46b9c0bae91479a7db1
Tanyaporn Wilaikaew <tanyapornwilaikaew@prontomac0024.saiyajin.com>
2014-07-31 12:01:09 +0700

    add unittest

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,6 +8,11 @@ class FizzBuzzTest(unittest.TestCase):
         result = fizzbuzz.take(3)
         self.assertEqual(result, "FIZZ")
 
+    def test_number_divisible_by_5_return_buzz(self):
+        fizzbuzz = FizzBuzz()
+        result = fizzbuzz.take(5)
+        self.assertEqual(result, "BUZZ")
+
 
 class FizzBuzz:
     def take(self, number):
