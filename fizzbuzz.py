878f40c699d30761fd4938398423940b80ce30cf
Tanyaporn Wilaikaew <tanyapornwilaikaew@prontomac0024.saiyajin.com>
2014-07-31 11:36:51 +0700

    divisible 3 return FIZZ

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class FizzBuzzTest(unittest.TestCase):
+    def test_number_divisible_by_3_return_fizz(self):
+        fizzbuzz = FizzBuzz()
+        result = fizzbuzz.take(3)
+        self.assertEqual(result, "FIZZ")
+
+
+class FizzBuzz:
+    def take(self, number):
+        return "FIZZ"
+
+if __name__ == "__main__":
+    unittest.main()
